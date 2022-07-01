45e792f338db63efeccb5b2e2cdb4c8e328a4e2d
Anderson <andeersonr@gmail.com>
2022-07-01 17:44:43 -0500

    initial main

---
+++ pages/rooms/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function rooms() {
+  return (
+    <>
+      <h1>Estas son las Habitaciones</h1>
+      <Link href="/">Go Home</Link>
+    </>
+  );
+}
